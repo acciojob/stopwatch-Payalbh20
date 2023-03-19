235c17ac93d9a151a0912ba1704f0e74a5e383b0
Payalbh20 <117173906+Payalbh20@users.noreply.github.com>
2023-03-19 10:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,66 +1,84 @@
-//your code here
-let timerInterval;
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const stopButton = document.getElementById('stop');
+const timeDisplay = document.getElementById('time');
 
-const padZero = (num) => {
-  if (num < 10) {
-    return `0${num}`;
-  }
-  return num;
-};
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval = null;
 
-const startTimer = () => {
+function startTimer() {
+  // Disable start button and enable pause and stop buttons
+  startButton.disabled = true;
+  pauseButton.disabled = false;
+  stopButton.disabled = false;
+
+  // Get current time in milliseconds
+  startTime = Date.now() - elapsedTime;
+
+  // Start timer
   timerInterval = setInterval(() => {
-    seconds++;
-    if (seconds === 60) {
-      minutes++;
-      seconds = 0;
-    }
-    if (minutes === 60) {
-      hours++;
-      minutes = 0;
-    }
-    const timerText = `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`;
-    document.getElementById("timer").textContent = timerText;
-  }, 1000);
-};
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+    updateTimeDisplay();
+  }, 10);
+}
 
-const stopTimer = () => {
+function pauseTimer() {
+  // Change pause button text to "continue"
+  pauseButton.textContent = 'continue';
+
+  // Pause timer
   clearInterval(timerInterval);
-  hours = 0;
-  minutes = 0;
-  seconds = 0;
-  document.getElementById("timer").textContent = "00:00:00";
-  document.getElementById("start-btn").disabled = false;
-  document.getElementById("pause-btn").disabled = true;
-  document.getElementById("pause-btn").textContent = "Pause";
-  document.getElementById("stop-btn").disabled = true;
-};
+}
 
-const pauseTimer = () => {
+function continueTimer() {
+  // Change pause button text back to "pause"
+  pauseButton.textContent = 'pause';
+
+  // Resume timer
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+    updateTimeDisplay();
+  }, 10);
+}
+
+function stopTimer() {
+  // Reset elapsed time and disable pause and stop buttons
+  elapsedTime = 0;
+  pauseButton.disabled = true;
+  stopButton.disabled = true;
+
+  // Stop timer and update time display
   clearInterval(timerInterval);
-  document.getElementById("start-btn").disabled = false;
-  document.getElementById("pause-btn").textContent = "Continue";
-};
+  updateTimeDisplay();
 
-document.getElementById("start-btn").addEventListener("click", () => {
-  startTimer();
-  document.getElementById("start-btn").disabled = true;
-  document.getElementById("pause-btn").disabled = false;
-  document.getElementById("stop-btn").disabled = false;
-});
+  // Enable start button
+  startButton.disabled = false;
+}
 
-document.getElementById("pause-btn").addEventListener("click", () => {
-  if (document.getElementById("pause-btn").textContent === "Pause") {
+function updateTimeDisplay() {
+  // Convert elapsed time to hours, minutes, and seconds
+  const hours = Math.floor(elapsedTime / 3600000).toString().padStart(2, '0');
+  const minutes = Math.floor((elapsedTime % 3600000) / 60000).toString().padStart(2, '0');
+  const seconds = Math.floor((elapsedTime % 60000) / 1000).toString().padStart(2, '0');
+
+  // Update time display
+  timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Initialize time display
+updateTimeDisplay();
+
+// Add event listeners to buttons
+startButton.addEventListener('click', startTimer);
+pauseButton.addEventListener('click', () => {
+  if (pauseButton.textContent === 'pause') {
     pauseTimer();
   } else {
-    startTimer();
-    document.getElementById("pause-btn").textContent = "Pause";
+    continueTimer();
   }
 });
-
-document.getElementById("stop-btn").addEventListener("click", () => {
-  stopTimer();
-});
+stopButton.addEventListener('click', stopTimer);
